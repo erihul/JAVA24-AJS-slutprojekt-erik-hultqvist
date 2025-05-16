9921a399237e790c09b528d6c6a914cc7bfb1a5a
Erik <erik.hultqvist@gritacademy.se>
2025-05-16 08:26:07 +0200

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+
+function App() {
+    return(
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+const root = createRoot ( document.querySelector('#root') );
+
+root.render(<App/>)
